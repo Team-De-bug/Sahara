50e3dc5db46a0d29ad81f32008a4cfb1b65ced3e
marudhu <marudhupaandian@gmail.com>
2019-11-23 20:46:39 +0530

    added database and made it accessible from admin

--- mysite/shop/models.py
+++ mysite/shop/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Stock(models.Model):
+    product_name = models.CharField(max_length=100, name="product_name")
+    cost = models.IntegerField(name="cost")
+    rating = models.IntegerField(name="rating")
+
+    def __str__(self):
+        return self.product_name
