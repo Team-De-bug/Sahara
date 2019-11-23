5fad838ff230eb18ed733fc898ef07f98d71a8ff
marudhu <marudhupaandian@gmail.com>
2019-11-23 21:34:38 +0530

    added homepage

--- mysite/shop/views.py
+++ mysite/shop/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Stock
+
 
 # Create your views here.
+def index(request):
+    items = Stock.objects.all()
+    return render(request, "sahara/item.html", {'items': items})
