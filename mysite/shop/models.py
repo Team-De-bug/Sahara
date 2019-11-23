5fdccb4addc9008862b5e34f2acbb949754db308
marudhu <marudhupaandian@gmail.com>
2019-11-23 23:06:01 +0530

    added columns to table

--- mysite/shop/models.py
+++ mysite/shop/models.py
@@ -6,6 +6,9 @@ class Stock(models.Model):
     product_name = models.CharField(max_length=100, name="product_name")
     cost = models.IntegerField(name="cost")
     rating = models.IntegerField(name="rating")
+    quantity = models.IntegerField(name="quantity", default=1)
+    description = models.TextField(name="description", default="")
+    seller = models.CharField(max_length=100, name="seller", default="sahara")
 
     def __str__(self):
         return self.product_name
