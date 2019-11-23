f69feb2a870292ad21925656ef48a05c8d195e98
marudhu <marudhupaandian@gmail.com>
2019-11-23 22:11:39 +0530

    added login and signup

---
+++ mysite/user/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def signup(request):
+    return render(request, 'user/signup.html')
+
+
+def login(request):
+    return render(request, 'user/login.html')
