5b311ef017a7e07a692bb1499b29f06d2a7a5181
RishiMenon2004 <rishimenon82004@gmail.com>
2019-11-24 18:56:23 +0530

    using sqlite for test

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -77,13 +77,18 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'sahara',
-        'USER': 'root',
-        'PASSWORD': 'Say Your Name!23',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+
+    #'default': {
+        #'ENGINE': 'django.db.backends.mysql',
+        #'NAME': 'sahara',
+        #'USER': 'root',
+        #'PASSWORD': 'Say Your Name!23',
+        #'HOST': 'localhost',
+        #'PORT': '3306',
+    #}
 }
 
 
