c6b4d76c89f48aee4e673afcff0bd11e35083ec4
marudhu <marudhupaandian@gmail.com>
2019-11-26 23:41:34 +0530

    added add and remove functionality to cart

--- mysite/shop/views.py
+++ mysite/shop/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Stock
+from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from user.models import Cart
+from django.http import HttpResponse
 
+from django.contrib.auth.models import User
+from .models import Stock
+from user.models import Order, Cart
 
 # Create your views here.
 def index(request):
@@ -10,8 +12,42 @@ def index(request):
     return render(request, "sahara/index.html", {'items': items})
 
 
-def item(request):
-    return render(request, "sahara/item.html")
+def place(request):
+
+    if request.method == "GET":
+        ID = request.GET['product_id']
+        item = Stock.objects.filter(id=ID)
+        item = item[0]
+        user = User.objects.filter(username=request.user)
+        user = user[0]
+        orders = user.cart.order_set.all()
+        in_cart = False
+        for order in orders:
+            if item == order.product:
+                print("in cart")
+                in_cart = True
+                order.quantity += 1
+                order.save()
+                break
+
+        if not in_cart:
+            order = Order(cart=user.cart, product=item)
+            order.save()
+            user.cart.order_set.add(order)
+        #user.save()
+    return HttpResponse("success")
+
+
+def remove(request):
+
+    if request.method == "GET":
+        ID = request.GET['order_id']
+        print(ID)
+        order = Order.objects.filter(id=ID)
+        order = order[0]
+        order.delete()
+
+    return HttpResponse("success")
 
 
 @login_required()
