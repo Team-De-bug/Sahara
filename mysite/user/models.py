2e8fd5d86abe53ef06424ca85df7d188962da25d
RishiMenon2004 <rishimenon82004@gmail.com>
2019-11-26 21:45:09 +0530

    added form to profile.html

--- mysite/user/models.py
+++ mysite/user/models.py
@@ -6,7 +6,7 @@ from shop.models import Stock
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_pic = models.ImageField(default='sahara-1.png', upload_to='profile_pics', name="profile_pic")
+    profile_pic = models.ImageField(default='sahara-2.png', upload_to='profile_pics', name="profile_pic")
 
     def __str__(self):
         return f'{self.user.username} Profile'
