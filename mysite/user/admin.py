38c9e77bb62deeb8b9a6e217ba5e3ab13cb4bbb8
marudhu <marudhupaandian@gmail.com>
2019-11-29 00:28:37 +0530

    changed admin site title and header

--- mysite/user/admin.py
+++ mysite/user/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from .models import Profile, Cart, Order
 
-
+admin.sites.AdminSite.index_title = 'Sahara Admin'
+admin.sites.AdminSite.site_header = "Sahara Admin"
 admin.site.register(Profile)
 admin.site.register(Cart)
 admin.site.register(Order)
